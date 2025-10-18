d114de40d0d95ee12f7452de9c089c6f6aa77200
pk-628996 <prince736804545@gmail.com>
2025-10-18 15:58:27 +0530

    Create pyqsh.js

---
+++ libs/pyqsh.js
@@ -0,0 +1,9 @@
+/*<img src="https://i.ibb.co/mVnW0QX3/Mid-Sem25-Sem1-Maths1.jpg" alt="Mid-Sem25-Sem1-Maths1" border="0">
+        <img src="https://i.ibb.co/svsL2DR5/Mid-Sem25-Sem1-Engineering-Graphics.jpg" alt="Mid-Sem25-Sem1-Engineering-Graphics" border="0">
+        <img src="https://i.ibb.co/1t1dhcZ0/Mid-Sem25-Sem1-Applied-Chemistry.jpg" alt="Mid-Sem25-Sem1-Applied-Chemistry" border="0">
+        <img src="https://i.ibb.co/bRr2w6bj/2of2-Mid-Sem25-Sem1-Communication-Skill.jpg" alt="2of2-Mid-Sem25-Sem1-Communication-Skill" border="0">
+        <img src="https://i.ibb.co/q3Wr16n9/Mid-Sem25-Sem1-Physics1.jpg" alt="Mid-Sem25-Sem1-Physics1" border="0">
+        <img src="https://i.ibb.co/RTzytxnz/1of2-Mid-Sem25-Sem1-Communication-Skill.jpg" alt="1of2-Mid-Sem25-Sem1-Communication-Skill" border="0">
+        
+
+const pyq_data = */
